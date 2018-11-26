4321c4a513c0ed2d5d3f590c94926dfeeee1c3c6
François Noël <fno@ogone.com>
2018-11-26 17:38:09 +0100

    Modify the world!

--- TestConsoleAppForCore/Program.cs
+++ TestConsoleAppForCore/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleAppForCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello modified World!");
         }
     }
 }
